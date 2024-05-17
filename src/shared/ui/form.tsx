5c324c5734e5b6801c75643d1ccbb068d1568210
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-17 13:18:08 +0300

    feat(shadcn): generate component for radio group & soner

--- src/shared/ui/form.tsx
+++ src/shared/ui/form.tsx
@@ -3,8 +3,8 @@ import { Slot } from '@radix-ui/react-slot';
 import * as React from 'react';
 import { Controller, ControllerProps, FieldPath, FieldValues, FormProvider, useFormContext } from 'react-hook-form';
 
-import { Label } from 'src/shared/ui/label';
 import { cn } from '~/shared/lib';
+import { Label } from '~/shared/ui/label';
 
 const Form = FormProvider;
 
