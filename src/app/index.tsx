f8e805d0e41cf9b8bc9b66a0ef045bf6fc6d80cc
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-17 06:07:01 +0300

    feat(router): add RouterProvider and base of router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.css';
-import { QueryProvider } from './providers';
+import { BrowserRouterProvider, QueryProvider } from './providers';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryProvider></QueryProvider>
+    <QueryProvider>
+      <BrowserRouterProvider />
+    </QueryProvider>
   </React.StrictMode>,
 );
--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,1 +1,2 @@
+export { BrowserRouterProvider } from './BrowserRouterProvider';
 export { QueryProvider } from './QueryProvider';
