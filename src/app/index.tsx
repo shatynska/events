452d8dc3b673799f1f5f21cc870ad73bea30a9f5
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-17 05:39:44 +0300

    feat(tanstack-query): add QueryProvider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.css';
+import { QueryProvider } from './providers';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryProvider></QueryProvider>
+  </React.StrictMode>,
+);
