2c7490131710eaf8a91bc13508ee93bff84ab0f0
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-17 06:22:37 +0300

    feat(root-layout): add root page and use it in router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,3 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { RootPage } from '~/pages/root';
 
-export const router = createBrowserRouter([]);
+export const router = createBrowserRouter([
+  {
+    id: 'root',
+    path: '/',
+    element: <RootPage />,
+  },
+]);
---
+++ src/pages/root/ui/RootPage.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+import { Header } from '~/widgets/header';
+
+export function RootPage() {
+  return (
+    <div className="m-auto flex max-w-screen-lg flex-col justify-center">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
